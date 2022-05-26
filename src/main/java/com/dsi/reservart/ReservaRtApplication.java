214d0b655cd84d573ab76b6c23a40df0ef188eb0
Alejo <alejoassain00@gmail.com>
2022-05-25 21:00:57 -0300

    primer commit

---
+++ src/main/java/com/dsi/reservart/ReservaRtApplication.java
@@ -0,0 +1,13 @@
+package com.dsi.reservart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaRtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaRtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsi/reservart/ReservaRtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsi.reservart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaRtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
